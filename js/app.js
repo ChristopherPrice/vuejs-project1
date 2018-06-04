c7fb8b239cea9a67d5966c358398869bf2f1602f
Chris Price <christopher.allen.price@gmail.com>
2018-06-04 17:03:44 -0400

    Add initial actions, styles

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@ new Vue({
         },
         player2Stats: {
             health: 100
+        },
+        currentPlayerIsPlayer1: true
+    },
+    methods: {
+        startNewGame: function () {
+            this.player1Stats.health = 100;
+            this.player2Stats.health = 100;
+        },
+        attack: function () {
+            var attackImpact = Math.floor(Math.random() * 10 + 1);
+            
+            if (this.currentPlayerIsPlayer1) {
+                this.player2Stats.health -= attackImpact;
+            } else {
+                this.player1Stats.health -= attackImpact;
+            }
         }
     }
 });
